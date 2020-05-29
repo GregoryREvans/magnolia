afdaae42910644fc7e2133641cb745f2af57e8a6
GregoryREvans <gregoryrowlandevans@gmail.com>
2020-05-29 11:04:43 -0400

    Mypy.

--- magnolia/Materials/rhythm/Segment_I/rhythm_handlers.py
+++ magnolia/Materials/rhythm/Segment_I/rhythm_handlers.py
@@ -7,12 +7,12 @@ from magnolia.Materials.rhythm.Segment_I.rhythm_makers import (
     rmaker_two,
 )
 
-silence_maker = abjadext.rmakers.stack(
+silence_maker_ = abjadext.rmakers.stack(
     abjadext.rmakers.NoteRhythmMaker(),
     abjadext.rmakers.force_rest(abjad.select().leaves(pitched=True)),
 )
 
-silence_maker = evans.RhythmHandler(rmaker=silence_maker, name="silence maker")
+silence_maker = evans.RhythmHandler(rmaker=silence_maker_, name="silence maker")
 
 rhythm_handler_one = evans.RhythmHandler(
     rmaker=rmaker_one, continuous=True, name="rhythm_handler_one"
